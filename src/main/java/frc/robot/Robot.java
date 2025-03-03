19764774f4c2982938c1b5da5509a8ecedda6bb1
WesleyP2006 <wesleypilamung@gmail.com>
2025-03-03 11:40:37 -0500

    Test commit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,6 +15,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 public class Robot extends TimedRobot {
+  //testing git
 
   StructPublisher<Pose2d> publisher = NetworkTableInstance.getDefault()
   .getStructTopic("poseFromLL", Pose2d.struct).publish();
